7832c03195e0ec8a872047d10bce980b1f74db19
Ganesh Pund <ganupund7787@gmail.com>
2024-07-28 15:32:18 -0700

    index file path bug

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import mongoose from "mongoose";

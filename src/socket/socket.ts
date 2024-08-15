79374b2146292f3ca14ed18fdfc5cfbd0c1d656b
Ganesh Pund <ganupund7787@gmail.com>
2024-08-14 19:20:00 -0700

    add socket creadiancial

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -11,6 +11,7 @@ const io = new Server(server, {
   cors: {
     origin: [FRONTEND_PORT], // frontende URI https://civil-hub-frontend.vercel.app
     methods: ["GET", "POST"],
+    credentials: true,
   },
 });
 
